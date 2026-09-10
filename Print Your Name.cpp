5ff89ffd3e54ebe0e7190647902184d1b14c0a72
yousef-mohsen <UG_31201990@ics.tanta.edu.eg>
2026-09-10 19:18:28 +0300

    Create Print Your Name.cpp

---
+++ Print Your Name.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void PrintName(string Name) {
+
+	cout << "\n Your Name is: " << Name << endl;
+}
+
+
+int main() {
+
+
+	PrintName("Yousef");
+
+	return 0;
+}
