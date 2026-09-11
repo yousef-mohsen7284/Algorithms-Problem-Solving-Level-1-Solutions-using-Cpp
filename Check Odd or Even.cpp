d516e72cde20c713099f0b4ac3ee9598626c0036
yousef-mohsen <UG_31201990@ics.tanta.edu.eg>
2026-09-11 15:54:02 +0300

    Create Check Odd or Even.cpp

---
+++ Check Odd or Even.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+enum enNumberType { Odd = 1, Even = 2 };
+
+int ReadNumber() {
+	int Number;
+	cout << "Please Enter a Nubmer ?\n";
+	cin >> Number;
+	return Number;
+}
+
+enNumberType CheckNumberType(int Number) {
+
+	int Result = Number % 2;
+	if (Result == 0)
+		return enNumberType::Even;
+	else
+		return enNumberType::Odd;
+}
+
+void PrintNumberType(enNumberType NumberType) {
+
+		cout << "Number is Even\n";
+	else
+		cout << "Number is Odd\n";
+}
+
+
+
+
+int main() {
+
+	PrintNumberType(CheckNumberType(ReadNumber()));
+
+	return 0;
+}
