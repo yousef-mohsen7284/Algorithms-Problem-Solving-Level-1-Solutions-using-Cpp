43377eb3af5cb33c8bea8cf696a7fe7eceb03e9b
yousef-mohsen7284 <UG_31201990@ics.tanta.edu.eg>
2026-09-21 16:13:54 +0300

    Create Sum Odd Numbers From 1 to N Using Do.. While Statement.cpp

---
+++ Sum Odd Numbers From 1 to N Using Do.. While Statement.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+enum EnOddOrEven { Odd = 1, Even = 2 };
+
+int ReadNumber() {
+	
+	int Number;
+	cout << "Please Enter a Number?\n";
+	cin >> Number;
+	return Number;
+}
+
+EnOddOrEven CheckOddOrEven(int Number) {
+
+	if (Number % 2 != 0)
+		return EnOddOrEven::Odd;
+	else
+		return EnOddOrEven::Even;
+}
+
+int SumOddNumbersFrom1toN__UsingDoWhileStatement(int Number) {
+
+	int Sum = 0;
+	int Counter = 0;
+
+	cout << "\nSum Odd Numbers Using Do..While Loop: ";
+
+	do
+	{
+		Counter++;
+		if (CheckOddOrEven(Counter) == EnOddOrEven::Odd) {
+			Sum += Counter;
+		}
+	} while (Counter < Number);
+
+	return Sum;
+}
+
+	
+
+int main() {
+
+	int Number = ReadNumber();
+
+	cout << SumOddNumbersFrom1toN__UsingDoWhileStatement(Number);
+	
+
+	return 0;
+}
